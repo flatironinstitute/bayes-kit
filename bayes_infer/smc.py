a62cfa346ed7a54d49f7ed89ff74f9e296d1ad8a
Brian Ward <bward@flatironinstitute.org>
2023-01-04 09:38:33 -0500

    Clean up smc slightly

--- bayes_infer/smc.py
+++ bayes_infer/smc.py
@@ -4,6 +4,7 @@ from numpy.typing import ArrayLike, NDArray
 
 Vector = NDArray[np.float64]
 DensityFunction = Callable[[Vector], float]
+Kernel = Callable[[Vector, DensityFunction], ArrayLike]
 
 
 class TemperedLikelihoodSMC:
@@ -14,13 +15,13 @@ class TemperedLikelihoodSMC:
         sample_initial: Callable[[int], ArrayLike],
         log_likelihood: DensityFunction,
         log_prior: DensityFunction,
-        kernel: Callable[[Vector, DensityFunction], ArrayLike],
+        kernel: Kernel,
     ) -> None:
 
         self.M = M
         self.N = N
-        self.thetas = np.array([sample_initial(m) for m in range(M)])
-        self.D = len(self.thetas[0])
+        self.thetas = np.array(list(map(sample_initial, range(M))))
+        self.D = self.thetas.shape[1]
 
         self.log_prior = log_prior
         self.log_likelihood = log_likelihood
@@ -34,22 +35,22 @@ class TemperedLikelihoodSMC:
         for n in range(1, self.N + 1):
             self.transition(n)
 
+    def time(self, n: int) -> float:
+        return n / self.N
+
     def transition(self, n: int) -> None:
-        tminus1 = (n - 1) / self.N
-
         def lpminus1(theta):
-            return self.log_likelihood(theta) * tminus1 + self.log_prior(theta)
-
-        t = n / self.N
+            return self.log_likelihood(theta) * self.time(n - 1) + self.log_prior(theta)
 
         def lp(theta):
-            return self.log_likelihood(theta) * t + self.log_prior(theta)
+            return self.log_likelihood(theta) * self.time(n) + self.log_prior(theta)
 
         # note: try to do this in parallel
         for m in range(self.M):
             self.thetas[m] = np.asanyarray(
                 self.kernel(np.atleast_1d(self.thetas[m]), lpminus1), dtype=np.ndarray
             )
+
         # note: duplicating calls to lpminus1
         self.thetas = importance_resample(self.thetas, lpminus1, lp)
 
@@ -58,17 +59,17 @@ def importance_resample(
     thetas: Vector, lpminus1: DensityFunction, lp: DensityFunction
 ) -> Vector:
     weights = np.exp(
-        np.apply_along_axis(
-            lambda theta: lp(theta) - lpminus1(theta), axis=1, arr=thetas
-        )
+        np.apply_along_axis(lp, axis=1, arr=thetas)
+        - np.apply_along_axis(lpminus1, axis=1, arr=thetas)
     )
-    # note: should use Generator object
+    # note: should use random Generator object
     M = thetas.shape[0]
     idxs = np.random.choice(M, size=M, replace=True, p=weights / weights.sum())
+
     return thetas[idxs]
 
 
-def metropolis_kernel(scale: float) -> Callable[[Vector, DensityFunction], Vector]:
+def metropolis_kernel(scale: float) -> Kernel:
     def proposal_rng(theta: Vector) -> Vector:
         return np.random.normal(loc=theta, scale=scale)
 
