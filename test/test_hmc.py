2cbd2a21423e7c4aff5dadbc46c6513a98254ac6
Brian Ward <bward@flatironinstitute.org>
2023-01-11 15:28:04 -0500

    Add basic HMC test

--- test/test_hmc.py
+++ test/test_hmc.py
@@ -1,14 +1,14 @@
 from models.std_normal import StdNormal
-from bayes_infer.mala import MALA
+from bayes_infer.hmc import HMCDiag
 import numpy as np
 
-def test_mala():
+def test_hmc_diag():
     # init with draw from posterior
     init = np.random.normal(loc=0, scale=1, size=[1])
     model = StdNormal()
-    mala = MALA(model, 0.3, init)
+    mala = HMCDiag(model, steps=10, stepsize=0.1, init=init)
 
-    M = 10000
+    M = 1000
     draws = np.array([mala.sample()[0] for _ in range(M)])
 
     mean = draws.mean(axis=0)
@@ -16,8 +16,3 @@ def test_mala():
 
     np.testing.assert_allclose(mean, 0, atol=0.1)
     np.testing.assert_allclose(var, 1, atol=0.1)
-
-    print(f"{draws[1:10]=}")
-    print(f"{mean=}  {var=}")
-
-
