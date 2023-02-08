Fix tests. Add finite-diff gradient to skewnorm model.

test/models/skew_normal.py
@@ -1,35 +1,37 @@
 import numpy.typing as npt
 import numpy as np
 from scipy import stats as sst
-from typing import Union
+from typing import Optional, Union
 from numpy.typing import NDArray
 from numpy import float64
 
 
 class SkewNormal:
-    def __init__(self, a: float = 4, loc: Union[float, NDArray[float64]] = 0) -> None:
+    def __init__(
+        self,
+        a: float = 4,
+        loc: Optional[Union[float, NDArray[float64]]] = None,
+        finite_difference_epsilon: float = 0.000001,
+    ) -> None:
         self.a = a
-        self.loc = loc
+        self._loc = loc if loc is not None else np.array([0])
+        self._epsilon = finite_difference_epsilon
 
     def dims(self) -> int:
         return 1
 
     def log_density(self, params_unc: npt.NDArray[np.float64]) -> float:
-        return sst.skewnorm.logpdf(params_unc, self.a, loc=self.loc)[0]
+        return sst.skewnorm.logpdf(params_unc, self.a, loc=self._loc)[0]
 
     def log_density_gradient(
         self, params_unc: npt.NDArray[np.float64]
     ) -> tuple[float, npt.NDArray[np.float64]]:
-        raise NotImplementedError
+        lp = self.log_density(params_unc)
+        lp_plus_epsilon = self.log_density(params_unc + self._epsilon)
+        return lp, np.array([(lp - lp_plus_epsilon)/self._epsilon])
 
     def posterior_mean(self) -> float:
-        mean: Union[float, NDArray[float64]] = sst.skewnorm.mean(self.a, loc=self.loc)  # type: ignore # scipy is not typed
-        if isinstance(mean, float):
-            return mean
-        return mean[0]
+        return sst.skewnorm.mean(self.a, loc=self._loc)  # type: ignore # scipy is not typed
 
     def posterior_variance(self) -> float:
-        var: Union[float, NDArray[float64]] = sst.skewnorm.var(self.a, loc=self.loc)  # type: ignore # scipy is not typed
-        if isinstance(var, float):
-            return var
-        return var[0]
+        return sst.skewnorm.var(self.a, loc=self._loc)  # type: ignore # scipy is not typed

test/test_metropolis.py
@@ -82,7 +82,7 @@ def test_metropolis_hastings_accept_test_accepts_more_likely_proposal_given_tran
 
 
 def test_metropolis_hastings_accept_test_reduces_to_metropolis_given_equal_transition_likelihoods():
-    M = 10000
+    M = 1000
     mock_uniform = Mock()
     for _ in range(M):
         lp_proposal = np.log(np.random.uniform())
@@ -130,7 +130,7 @@ def test_metropolis_beta_binom() -> None:
 
 
 def test_metropolis_hastings_skew_normal() -> None:
-    M = 50000  # This is obviously extremely slow, can we increase the tolerance?
+    M = 2500
     skewness_a = 4
     model = SkewNormal(a=skewness_a)
     init: NDArray[np.float64] = sst.skewnorm.rvs(skewness_a, size=[1])  # type: ignore
@@ -146,8 +146,8 @@ def test_metropolis_hastings_skew_normal() -> None:
     mean = draws.mean(axis=0)
     var = draws.var(axis=0, ddof=1)
 
-    np.testing.assert_allclose(mean, model.posterior_mean(), atol=0.1)
-    np.testing.assert_allclose(var, model.posterior_variance(), atol=0.1)
+    np.testing.assert_allclose(mean, model.posterior_mean(), atol=0.3)
+    np.testing.assert_allclose(var, model.posterior_variance(), atol=0.3)
 
 
 def test_metropolis_reproducible() -> None:
@@ -267,16 +267,23 @@ def test_metropolis_hastings_iter_returns_self() -> None:
     assert i == mh
 
 
-# Don't actually try to sample anything, just confirm that the
-# sample method was called when iterating over the MetropolisHasting object
-@patch.object(MetropolisHastings, "sample")
-def test_metropolis_hastings_next_calls_sample(mock_sample) -> None:
+def test_metropolis_hastings_next_trajectory_matches_calling_sample() -> None:
     model = StdNormal()
-    proposal_fn = lambda x: 1
-    transition_lp_fn = lambda x, y: 1
-    mh = MetropolisHastings(model, proposal_fn, transition_lp_fn)
-    _ = [next(mh) for i in range(1)]
-    mock_sample.assert_called_once()
+    init = np.random.normal(loc=0, scale=1, size=[1])
+    M = 25
+
+    proposal_generator = np.random.default_rng(seed=123)
+    p_fn = lambda theta: proposal_generator.normal(loc=theta, scale=4)
+    x_fn = lambda o, g: 1  # symmetric likelihood--don't bother to compute
+    mh1 = MetropolisHastings(model, proposal_fn=p_fn, transition_lp_fn=x_fn, init=init, seed=996)
+    draws_1 = np.array([mh1.sample()[0] for _ in range(M)])
+
+    proposal_generator = np.random.default_rng(seed=123)
+    p_fn = lambda theta: proposal_generator.normal(loc=theta, scale=4)
+    mh2 = MetropolisHastings(model, proposal_fn=p_fn, transition_lp_fn=x_fn, init=init, seed=996)
+    draws_2 = np.array([next(mh2)[0] for _ in range(M)])
+
+    np.testing.assert_array_equal(draws_1, draws_2)
 
 
 def test_metropolis_hastings_throws_when_proposal_fn_generates_wrong_type() -> None: