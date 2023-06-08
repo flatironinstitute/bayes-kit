b32c4be308cbecfc2bb460e34fd6600e9e052364
gil2rok <gilad.turok@gmail.com>
2023-06-07 20:20:58 -0400

    begin testing

--- bayes_kit/drhmc.py
+++ bayes_kit/drhmc.py
@@ -1,4 +1,4 @@
-from typing import Iterator, Optional, Union
+from typing import Iterator, Optional, Union, Tuple, Callable
 from numpy.typing import NDArray
 import numpy as np
 
@@ -12,8 +12,8 @@ class DRHMC:
         self,
         model: GradModel,
         num_proposals: int,
-        stepsize: Union[float, NDArray[np.float64], function] = None,
-        steps: Union[float, NDArray[np.float64], function] = None,
+        stepsize_fn: Callable,
+        steps_fn: Callable,
         metric_diag: Optional[NDArray[np.float64]] = None,
         init: Optional[NDArray[np.float64]] = None,
         seed: Union[None, int, np.random.BitGenerator, np.random.Generator] = None,
@@ -21,8 +21,10 @@ class DRHMC:
         self._model = model
         self._dim = self._model.dims()
         self._num_proposals = num_proposals
-        self._stepsize = stepsize # TODO: add function
-        self._steps = steps # TODO: add function
+        self._stepsize_fn = stepsize_fn
+        self._steps_fn = steps_fn
+        self._stepsize_list = []
+        self._steps_list = []
         self._metric = metric_diag or np.ones(self._dim)
         self._rng = np.random.default_rng(seed)
         self._theta = (
@@ -30,7 +32,6 @@ class DRHMC:
             if (init is not None and init.shape != (0,))
             else self._rng.normal(size=self._dim)
         )
-        self._rho = self._rng.normal(size=self._dim)
 
     def __iter__(self) -> Iterator[Draw]:
         return self
@@ -55,27 +56,44 @@ class DRHMC:
         rho = rho_mid + 0.5 * stepsize * np.multiply(self._metric, grad)
         return (theta, rho)
     
-    def get_stepsize(self) -> float:
-        self._stepsizes.append(self._stepsize) 
-        return self._stepsizes[-1]
+    def get_stepsize(self, *args) -> float:
+        stepsize = self._stepsize_fn(*args)
+        self._stepsize_list.append(stepsize)
+        return stepsize
     
-    def get_steps(self) -> int:
-        self._steps.append(self._steps) 
-        return self._steps[-1]
+    def get_steps(self, *args) -> int:
+        steps = self._steps_fn(*args)
+        self._steps_list.append(steps) 
+        return steps
     
     def sample(self) -> Draw:
-        logp = self.joint_logp(self._theta, self._rho)
-        if self._num_proposals == 0:
-            return self._theta, logp
-        self._num_proposals -= 1
-        
-        stepsize, steps = self.get_stepsize(), self.get_steps()
-        theta_prop, rho_prop = self.leapfrog(self._theta, self._rho, stepsize, steps)
+        theta, rho = self._theta, self._rng.normal(size=self._dim)
+        for k in range(self._num_proposals):
+            stepsize, steps = self.get_stepsize(k), self.get_steps(k)
+            theta_prop, rho_prop = self.leapfrog(theta, rho, stepsize, steps)
+            accept_logp = self.accept(theta, rho, theta_prop, rho_prop, k)
+            print(np.exp(accept_logp))
+            if np.log(self._rng.uniform()) < accept_logp:
+                self._theta, logp_prop = theta_prop, self.joint_logp(theta_prop, rho_prop)
+                return self._theta, logp_prop
+        logp = self.joint_logp(theta, rho)
+        return self._theta, logp
+
+    def accept(
+        self, theta: NDArray[np.float64], rho: NDArray[np.float64], 
+        theta_prop: NDArray[np.float64], rho_prop: NDArray[np.float64], k: int
+    ) -> float:
+        logp = self.joint_logp(theta, rho)
         logp_prop = self.joint_logp(theta_prop, rho_prop)
-        
-        
-        
-        if np.log(self._rng.uniform()) < logp_prop - logp:
-            return theta_prop, logp_prop
-        else:
-            return self._theta, logp
+        log_num, log_denom = 0, 0
+        for i in range(k):
+            stepsize, steps = self._stepsize_list[i], self._steps_list[i]
+            
+            theta_ghost, rho_ghost = self.leapfrog(theta_prop, rho_prop, stepsize, steps)
+            accept_logp = self.accept(theta_prop, rho_prop, theta_ghost, rho_ghost, i)
+            log_num += np.log(1 - np.exp(accept_logp))
+            
+            theta_reject, rho_reject = self.leapfrog(theta, rho, stepsize, steps)
+            accept_logp = self.accept(theta, rho, theta_reject, rho_reject, i)
+            log_denom += np.log(1 - np.exp(accept_logp))
+        return (logp_prop - logp) + (log_num - log_denom)
