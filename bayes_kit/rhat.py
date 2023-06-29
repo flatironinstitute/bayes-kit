0cd172451f1513641a592c1d4fc1ead9ffa78be4
Bob Carpenter <carp@alias-i.com>
2023-06-29 18:17:55 -0400

    rank norm rhat

--- bayes_kit/rhat.py
+++ bayes_kit/rhat.py
@@ -1,9 +1,86 @@
 import numpy as np
 from numpy.typing import NDArray, ArrayLike
+import scipy as sp
 
 FloatType = np.float64
 VectorType = NDArray[FloatType]
 SeqType = ArrayLike
+
+
+def split_chains(chains: list[SeqType]) -> list[SeqType]:
+    """
+    Return a list of the input chains split in half.  The result will be
+    a list twice as long as the input.  For example,
+    ```
+    >>> split_chains([[1, 2, 3, 4], [5, 6, 7]])
+    [[1, 2], [3, 4], [5, 6], [7]]
+    ```
+
+    Parameters:
+    chains: List of univariate Markov chains.
+
+    Returns:
+    List of input chains split in half.
+    """
+    return [list(arr) for chain in chains for arr in np.array_split(chain, 2)]
+
+
+def rank_chains(chains: list[SeqType]) -> list[SeqType]:
+    """
+    Returns a copy of the included Markov chains transformed with
+    ranks normalized to [0, 1] and an offset inverse CDF. The ranks
+    are ascending and start with 1 for the smallest value.
+
+    Parameters:
+    chains: list of univariate Markov chains
+
+    Returns:
+    List of chains with values replaced by transformed ranks.
+    """
+    if len(chains) == 0:
+        return chains
+    flattened = np.concatenate(chains)
+    ranks = flattened.argsort().argsort() + 1
+    reshaped_arrays = []
+    current_index = 0
+    for array in chains:
+        size = len(array)
+        reshaped_arrays.append(ranks[current_index : current_index + size])
+        current_index += size
+    return reshaped_arrays
+
+
+def rank_normalize_chains(chains: list[SeqType]) -> list[SeqType]:
+    """Return the rank-normalized version of the input chains.
+
+    The rank-normalized value for element `j` of list `i` is
+    ```
+    inverse_Phi((rank[i][j] - 3/8) / (size(chains) - 1/4),
+    ```
+    where `inv_Phi` is the inverse cumulative distribution function for
+    the standard normal distribution and
+    ```
+    rank[i][j] = rank_chains(chains)[i][j]
+    ```
+    is the rank of element `i` in chain `j` and `size(chains)` is the
+    total number of elements in the chains.
+
+    For a specification of ranking, see :func:`rank_chains`.
+
+    Parameters:
+    chains: List of univariate Markov chains.
+
+    Returns:
+    List of chains with values replaced by rank-normalized values.
+    """
+    S = sum([len(chain) for chain in chains])
+    result = chains
+    ranked_chains = rank_chains(chains)
+    for i, chain_i in enumerate(ranked_chains):
+        for j, rank_ij in enumerate(chain_i):
+            val = sp.stats.norm.ppf((rank_ij - 0.325) / (S - 0.25))
+            result[i][j] = val
+    return result
 
 
 def rhat(chains: list[SeqType]) -> FloatType:
@@ -27,7 +104,8 @@ def rhat(chains: list[SeqType]) -> FloatType:
     R-hat statistic
 
     Throws:
-    ValueError: if there are fewer than two chains
+    ValueError: If there is not at least one chain or if any chain has
+    fewer than two elements.
     """
     if len(chains) < 2:
         raise ValueError(f"rhat requires len(chains) >= 2, but {len(chains) = }")
@@ -44,43 +122,59 @@ def rhat(chains: list[SeqType]) -> FloatType:
     return r_hat
 
 
-def split_chains(chains: list[SeqType]) -> list[SeqType]:
-    """
-    Return a list of the input chains split in half.  The result will be
-    a list twice as long as the input.  For example, given
-    ```
-    >>> split_chains([[1,2,3,4],[5,6, 7]])
-    [[1, 2], [3, 4], [5, 6], [7]]
-    ```
-
-    Parameters:
-    chains: list of univariate Markov chains
-
-    Returns:
-    List of input chains split in half.
-    """
-    return [list(arr) for chain in chains for arr in np.array_split(chain, 2)]
-
-
 def split_rhat(chains: list[SeqType]) -> FloatType:
     """
     Return the potential scale reduction factor (R-hat) for a list of
-    Markov chains consisting of each of the input chains split in half.  
+    Markov chains consisting of each of the input chains split in half.
     Unlike the base `rhat(chains)` function, this version is applicable
     to a single Markov chain.
 
-    Split R-hat was introduced in the *Stan Reference Manual.*  The 
+    Split R-hat was introduced in the *Stan Reference Manual.*  The
     first official publication was in the following book.
 
     Gelman, A., Carlin, J.B., Stern, H.S., Dunson, D.B., Vehtari,
     A. and Rubin, D.B., 2013. *Bayesian Data Analysis.* Third Edition.
-    CRC press. 
+    CRC press.
+
+    See :func:`split_chains` for a specification of splitting.
 
     Parameters:
-    chains: list of univariate Markov chains
+    chains: List of univariate Markov chains.
 
     Returns:
-    Split R-hat statistic
+    Split R-hat statistic.
+
+    Throws:
+    ValueError: If there are fewer than two chains or if any chain has
+    fewer than than four elements.
     """
     return rhat(split_chains(chains))
 
+
+def rank_normalized_rhat(chains: list[SeqType]) -> FloatType:
+    """Return the rank-normalized R-hat for the specified chains.
+    Rank normalized r-hat replaces each value in the chains with its
+    rank, applies a shifted inverse standard normal cdf, and
+    returns the split R-hat value of the result.
+
+    Rank-normalized R-hat was introduced in the following paper.
+
+    Vehtari, A., Gelman, A., Simpson, D., Carpenter, B. and Bürkner,
+    P.C., 2021. Rank-normalization, folding, and localization: An
+    improved R-hat for assessing convergence of MCMC (with
+    discussion). *Bayesian Analysis* 16(2):667-718.
+
+    See :func:`split_rhat` for a specification of split R-hat and
+    :func:`rank_normalize_chains` for rank normalization.
+
+    Parameters:
+    chains: List of univariate Markov chains.
+
+    Returns:
+    Rank-normalized R-hat statistic.
+
+    Throws:
+    ValueError: If there are fewer than two chains or if any chain has
+    fewer than than four elements.
+    """
+    return split_rhat(rank_normalize_chains(chains))
--- test/test_rhat.py
+++ test/test_rhat.py
@@ -1,5 +1,13 @@
 import numpy as np
-from bayes_kit.rhat import rhat, split_chains, split_rhat
+import scipy as sp
+from bayes_kit.rhat import (
+    rhat,
+    split_chains,
+    split_rhat,
+    rank_chains,
+    rank_normalize_chains,
+    rank_normalized_rhat,
+)
 import pytest as pt
 
 
@@ -30,33 +38,120 @@ def test_rhat_ragged():
     chain2 = [0.99, 1.00, 1.01, 1.15, 0.83, 0.95]
     chains = [chain1, chain2]
     rhat_est = rhat(chains)
-    np.testing.assert_allclose(rhat_expected(chains), rhat(chains),
-                                   atol=0.1, rtol=0.2)
+    np.testing.assert_allclose(rhat_expected(chains), rhat(chains), atol=0.1, rtol=0.2)
+
 
 def test_rhat_size_exceptions():
     bad1 = []
-    bad2 = [[1.01, 1.2, 1.3, 1.4]],
-    bad3 = [[1, 2, 3], [4]]
+    bad2 = ([[1.01, 1.2, 1.3, 1.4]],)
+    bad3 = [[1, 2, 3], [4], [5, 6, 7, 8, 9]]
     for chains in [bad1, bad2, bad3]:
         with pt.raises(ValueError):
             rhat(chains)
 
+
 def test_split_chains():
-    print("hello")
     np.testing.assert_equal([], split_chains([]))
     np.testing.assert_equal([[1], []], split_chains([[1]]))
     np.testing.assert_equal([[1], [2]], split_chains([[1, 2]]))
     np.testing.assert_equal([[1, 2], [3]], split_chains([[1, 2, 3]]))
-    np.testing.assert_equal([[1, 2, 3], [4, 5, 6]],
-                                split_chains([[1, 2, 3, 4, 5, 6]]))
-    np.testing.assert_equal([[1, 2], [3], [4, 5], [6, 7]],
-                                split_chains([[1, 2, 3], [4, 5, 6, 7]]))
+    np.testing.assert_equal([[1, 2, 3], [4, 5, 6]], split_chains([[1, 2, 3, 4, 5, 6]]))
+    np.testing.assert_equal(
+        [[1, 2], [3], [4, 5], [6, 7]], split_chains([[1, 2, 3], [4, 5, 6, 7]])
+    )
+
 
 def test_split_rhat():
-    np.testing.assert_allclose(rhat([[1, 2], [3, 4]]),
-                                   split_rhat([[1, 2, 3, 4]]))
-    np.testing.assert_allclose(rhat([[1, 2, 2], [3, 4, 3]]),
-                                   split_rhat([[1, 2, 2, 3, 4, 3]]))
-    np.testing.assert_allclose(rhat([[1, -2, 3], [4, 5, 6], [7, 8], [9, 12]]),
-                                   split_rhat([[1, -2, 3, 4, 5, 6], [7, 8, 9, 12]]))
-                            
+    np.testing.assert_allclose(rhat([[1, 2], [3, 4]]), split_rhat([[1, 2, 3, 4]]))
+    np.testing.assert_allclose(
+        rhat([[1, 2, 2], [3, 4, 3]]), split_rhat([[1, 2, 2, 3, 4, 3]])
+    )
+    np.testing.assert_allclose(
+        rhat([[1, -2, 3], [4, 5, 6], [7, 8], [9, 12]]),
+        split_rhat([[1, -2, 3, 4, 5, 6], [7, 8, 9, 12]]),
+    )
+
+
+def test_split_rhat_size_exceptions():
+    bad1 = []
+    bad2 = [[1.01, 1.2, 1.3]]
+    bad3 = [[1, 2, 3], [4, 5, 6, 7]]
+    for chains in [bad1, bad2, bad3]:
+        with pt.raises(ValueError):
+            split_rhat(chains)
+
+
+def test_rank_chains():
+    np.testing.assert_equal([], rank_chains([]))
+    np.testing.assert_equal([[1]], rank_chains([[2.3]]))
+    np.testing.assert_equal([[1, 2]], rank_chains([[2.3, 4.9]]))
+    np.testing.assert_equal([[2, 3, 1]], rank_chains([[3.9, 5.2, 2.1]]))
+    np.testing.assert_equal([[2], [1]], rank_chains([[4.2], [1.9]]))
+    np.testing.assert_equal([[2, 3], [1, 4]], rank_chains([[4.2, 5.7], [1.9, 12.2]]))
+    np.testing.assert_equal(
+        [[2, 3], [5, 4], [1, 6]], rank_chains([[4.2, 5.7], [7.2, 6.1], [-12.9, 107]])
+    )
+
+
+def rank_norm(r, S):
+    return sp.stats.norm.ppf((r - 0.325) / (S - 0.25))
+
+
+def test_rank_normalize_chains():
+    np.testing.assert_equal([], rank_normalize_chains([]))
+
+    rn11 = rank_norm(1, 1)
+    np.testing.assert_allclose([[rn11]], rank_normalize_chains([[32.7]]))
+    np.testing.assert_allclose([[rn11]], rank_normalize_chains([[-10.7]]))
+
+    rn12 = rank_norm(1, 2)
+    rn22 = rank_norm(2, 2)
+    np.testing.assert_allclose([[rn22, rn12]], rank_normalize_chains([[3.9, 1.8]]))
+
+    rn13 = rank_norm(1, 3)
+    rn23 = rank_norm(2, 3)
+    rn33 = rank_norm(3, 3)
+    np.testing.assert_allclose(
+        [[rn23, rn33, rn13]], rank_normalize_chains([[3.5, 5.9, 1.0]])
+    )
+
+    rn14 = rank_norm(1, 4)
+    rn24 = rank_norm(2, 4)
+    rn34 = rank_norm(3, 4)
+    rn44 = rank_norm(4, 4)
+    np.testing.assert_allclose(
+        [[rn34, rn24], [rn14, rn44]], rank_normalize_chains([[3.9, 3.1], [2.2, 5.9]])
+    )
+
+
+def test_rank_normalized_rhat_size_exceptions():
+    bad1 = []
+    bad2 = [[1.01, 1.2, 1.3]]
+    bad3 = [[1, 2, 3], [4]]
+    for chains in [bad1, bad2, bad3]:
+        with pt.raises(ValueError):
+            rank_normalized_rhat(chains)
+
+
+def test_rank_normalized_rhat():
+    rn14 = rank_norm(1, 4)
+    rn24 = rank_norm(2, 4)
+    rn34 = rank_norm(3, 4)
+    rn44 = rank_norm(4, 4)
+    np.testing.assert_allclose(
+        rhat([[rn14, rn44], [rn34, rn24]]),
+        rank_normalized_rhat([[1.8, 10.9, 6.3, 5.1]]),
+    )
+
+    rn18 = rank_norm(1, 8)
+    rn28 = rank_norm(2, 8)
+    rn38 = rank_norm(3, 8)
+    rn48 = rank_norm(4, 8)
+    rn58 = rank_norm(5, 8)
+    rn68 = rank_norm(6, 8)
+    rn78 = rank_norm(7, 8)
+    rn88 = rank_norm(8, 8)
+    np.testing.assert_allclose(
+        split_rhat([[rn28, rn38, rn78, rn88], [rn18, rn48, rn68, rn58]]),
+        rank_normalized_rhat([[2, 3, 7, 8], [1, 4, 6, 5]]),
+    )
