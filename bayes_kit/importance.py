df35f7290ce43cf84fe7726408d2724d92c514d8
Bob Carpenter <bcarpenter@flatironinstitute.org>
2023-12-08 17:08:07 -0500

    speed improve, psis refs

--- bayes_kit/importance.py
+++ bayes_kit/importance.py
@@ -1,14 +1,17 @@
 import numpy as np
 import scipy as sp
 
+def mapz(f, draws):
+    return np.apply_along_axis(f, arr=draws, axis = 0)
+
 def is_weights(proposal, target, num_draws):
     draws = [proposal.sample() for _ in range(num_draws)]
-    log_weights = [target.log_density(draw) - proposal.log_density(draw) for draw in draws]
+    log_weights = mapz(target.log_density, draws) - mapz(proposal.log_density, draws)
     weights = np.exp(log_weights - sp.special.logsumexp(log_weights))
     return draws, weights
 
 def is_expect(draws, weights, f):
-    return np.sum([weight * f(draw) for draw, weight in zip(draws, weights)], axis=0)
+    return (mapz(f, draws) * weights).sum(axis = -1)
 
 def importance_sample(proposal, target, num_draws, f):
     draws, weights = is_weights(proposal, target, num_draws)
@@ -17,10 +20,17 @@ def importance_sample(proposal, target, num_draws, f):
 def importance_resample(proposal, target, pool_size, num_draws):
     pool, weights = is_weights(proposal, target, pool_size)
     counts = np.random.multinomial(num_draws, weights)
-    draws = []
-    for draw, count in zip(pool, counts):
-        draws.extend([draw] * count)
-    return draws        
+    return np.repeat(pool, counts, axis=0)
+
+def generalized_pareto_estimate(x):
+    # A. Vehtari, D. Simpson, A. Gelman, Y. Yao, J. Gabry. 2022. Pareto smoothed
+    # importance sampling.  arXiv:1507.02646. https://arxiv.org/pdf/1507.02646.pdf 
+    # J. Zhang, M.A. Stephens. 2009. A new and efficient estimation method for
+    # the generalized Pareto distribution. Technometrics 51(3).
+    # https://www.jstor.org/stable/40586625 
+    theta_hat = 1
+    kappa_hat = 1
+    return theta_hat, kappa_hat
 
 def pareto_smooth(weights):
     kappa = 0.5
